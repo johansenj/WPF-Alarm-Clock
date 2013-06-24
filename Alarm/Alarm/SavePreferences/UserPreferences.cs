21fa4539210c22b7b27739aa5df9f77115fa9ceb
Jacob Johansen <johansenjuwp[at]gmail>
2013-06-23 21:14:10 -0500

    updated custome controller

---
+++ Alarm/Alarm/SavePreferences/UserPreferences.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alarm.SavePreferences
+{
+    class UserPreferences
+    {
+        private static readonly string saveRoot = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\MyCompanyName\\MyApplicationName";
+
+        public static int SaveSetting(string key, object value)
+        {
+            
+            return -1;
+        }
+
+        public static int GetSetting(string key, ref object value)
+        {
+
+            return -1;
+        }
+    }
+}
