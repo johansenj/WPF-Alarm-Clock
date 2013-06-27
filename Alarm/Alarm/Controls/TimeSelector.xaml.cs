ad613137954b4ca4d2bd704f97f8f47aacf0ae4c
Jacob Johansen <johansenjuwp[at]gmail>
2013-06-26 19:39:13 -0500

    button up and down now currently work on time select user control

--- Alarm/Alarm/Controls/TimeSelector.xaml.cs
+++ Alarm/Alarm/Controls/TimeSelector.xaml.cs
@@ -20,6 +20,8 @@ namespace Alarm
     /// </summary>
     public partial class TimeSelector : UserControl
     {
+        private bool hourHasFocus = true;
+
         public TimeSelector()
         {
             InitializeComponent();
@@ -36,6 +38,134 @@ namespace Alarm
 
             }
         }
+
+        /// <summary>
+        /// Used to verify that, as time is input, that it is valid, also to allow the use of the
+        /// arrow pad to move the hours up and down
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TxtHour_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// Used to verify that, as time is input, that it is valid, also to allow the use of the
+        /// arrow pad to move the minutes up and down
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtMin_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Used to keep track of which text box last had focus
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtMin_GotFocus(object sender, RoutedEventArgs e)
+        {
+            hourHasFocus = false;
+        }
+
+        /// <summary>
+        /// Used to keep track of which text box last had focus
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtHour_GotFocus(object sender, RoutedEventArgs e)
+        {
+            hourHasFocus = true;
+        }
+
+        /// <summary>
+        /// Used to move the hour or min up depending on which was last selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnUp_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (hourHasFocus)
+                {
+                    int workingHour = Convert.ToInt32(txtHour.Text);
+                    if (workingHour == 12)
+                    {
+                        workingHour = 1;
+                    }
+                    else
+                    {
+                        workingHour++;
+                    }
+                    txtHour.Text = workingHour.ToString();
+                }
+                else
+                {
+                    int workingHour = Convert.ToInt32(txtMin.Text);
+                    if (workingHour == 59)
+                    {
+                        workingHour = 1;
+                    }
+                    else
+                    {
+                        workingHour++;
+                    }
+                    txtMin.Text = workingHour.ToString("D2");
+                }
+            }
+            catch (Exception)
+            {
+                //TODO: Out put to log in user directory so users can send any errors to me if needed
+            }
+            
+        }
+
+        /// <summary>
+        /// Used to move the hour or min down depending on which was last selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDown_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (hourHasFocus)
+                {
+                    int workingHour = Convert.ToInt32(txtHour.Text);
+                    if (workingHour == 1)
+                    {
+                        workingHour = 12;
+                    }
+                    else
+                    {
+                        workingHour--;
+                    }
+                    txtHour.Text = workingHour.ToString();
+                }
+                else
+                {
+                    int workingHour = Convert.ToInt32(txtMin.Text);
+                    if (workingHour == 0)
+                    {
+                        workingHour = 59;
+                    }
+                    else
+                    {
+                        workingHour--;
+                    }
+                    txtMin.Text = workingHour.ToString("D2");
+                }
+            }
+            catch (Exception)
+            {
+                //TODO: Out put to log in user directory so users can send any errors to me if needed
+            }
+        }
+
 
     }
 }
