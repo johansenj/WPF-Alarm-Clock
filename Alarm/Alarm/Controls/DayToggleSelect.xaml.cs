3163db3295a3a78bf4f14a3031009ec57965b92c
Jacob Johansen <johansenjuwp[at]gmail>
2013-06-27 12:18:09 -0500

    fixed loading problem accidently add wrong style of component

--- Alarm/Alarm/Controls/DayToggleSelect.xaml.cs
+++ Alarm/Alarm/Controls/DayToggleSelect.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Alarm.Controls
+namespace Alarm
 {
     /// <summary>
     /// Interaction logic for DayToggleSelect.xaml
