5f7d34f6b667c7f2d1f4ba8e64c3c5a4bd419968
Jacob Johansen <johansenjuwp[at]gmail>
2013-06-28 02:23:59 -0500

    add empty templates that will be turned into push button toggle style and slide toggle styles, i am content with
    day toggle select control, except to apply the style when finished

--- Alarm/Alarm/Controls/DayToggleSelect.xaml.cs
+++ Alarm/Alarm/Controls/DayToggleSelect.xaml.cs
@@ -24,5 +24,126 @@ namespace Alarm
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Sets all toggle buttons to "off"
+        /// </summary>
+        public void resetControl()
+        {
+            tbSun.IsChecked = false;
+            tbMon.IsChecked = false;
+            tbTue.IsChecked = false;
+            tbWed.IsChecked = false;
+            tbThu.IsChecked = false;
+            tbFri.IsChecked = false;
+            tbSat.IsChecked = false;
+        }
+
+        /// <summary>
+        /// Used to get and set selected days, is returned as comma seperated string
+        /// </summary>
+        public string SelectedDays
+        {
+            get
+            {
+                string selected = "";
+                int totalDaysSelected = 0;
+
+                if ((bool)tbSun.IsChecked)
+                {
+                    selected += "Sun,";
+                    totalDaysSelected++;
+                }
+                if ((bool)tbMon.IsChecked)
+                {
+                    selected += "Mon,";
+                    totalDaysSelected++;
+                }
+                if ((bool)tbTue.IsChecked)
+                {
+                    selected += "Tue,";
+                    totalDaysSelected++;
+                }
+                if ((bool)tbWed.IsChecked)
+                {
+                    selected += "Wed,";
+                    totalDaysSelected++;
+                }
+                if ((bool)tbThu.IsChecked)
+                {
+                    selected += "Thu,";
+                    totalDaysSelected++;
+                }
+                if ((bool)tbFri.IsChecked)
+                {
+                    selected += "Fri,";
+                    totalDaysSelected++;
+                }
+                if ((bool)tbSat.IsChecked)
+                {
+                    selected += "Sat,";
+                    totalDaysSelected++;
+                }
+
+                if (totalDaysSelected == 7)
+                {
+                    selected = "Every Day";
+                }
+                else if (totalDaysSelected == 2 && (bool)tbSun.IsChecked && (bool)tbSat.IsChecked)
+                {
+                    selected = "Weekend";
+                }
+                else if (totalDaysSelected == 5 && !(bool)tbSun.IsChecked && !(bool)tbSat.IsChecked)
+                {
+                    selected = "Weekdays";
+                }
+                return selected;
+            }
+
+            set
+            {
+                bool everyday = value.Contains("Every Day");
+                bool weekend = value.Contains("Weekend");
+                bool weekdays = value.Contains("Weekdays");
+                bool sun = everyday || weekend || value.Contains("Sun");
+                bool mon = everyday || weekdays || value.Contains("Mon");
+                bool tue = everyday || weekdays || value.Contains("Tue");
+                bool wed = everyday || weekdays || value.Contains("Wed");
+                bool thu = everyday || weekdays || value.Contains("Thu");
+                bool fri = everyday || weekdays || value.Contains("Fri");
+                bool sat = everyday || weekend || value.Contains("Sat");
+
+                resetControl();
+
+                if (sun)
+                {
+                    tbSun.IsChecked = true;
+                }
+                if (mon)
+                {
+                    tbMon.IsChecked = true;
+                }
+                if (tue)
+                {
+                    tbTue.IsChecked = true;
+                }
+                if (wed)
+                {
+                    tbWed.IsChecked = true;
+                }
+                if (thu)
+                {
+                    tbThu.IsChecked = true;
+                }
+                if (fri)
+                {
+                    tbFri.IsChecked = true;
+                }
+                if (sat)
+                {
+                    tbSat.IsChecked = true;
+                }
+            }
+        }
     }
 }
